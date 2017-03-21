abc861b79f0757854eaf28bc884889437577af37
michaelryannorton <michael@michaelryannorton.com>
2017-03-20 20:10:19 -0700

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2797,
-	"created_at": "Wed Mar 08 03:10:19 +0000 2017",
+	"tweets": 2798,
+	"created_at": "Tue Mar 21 03:10:18 +0000 2017",
 	"lang": "en"
 }
--- tweets/data/js/tweets/2017_03.js
+++ tweets/data/js/tweets/2017_03.js
@@ -1,6 +1,204 @@
 Grailbird.data.tweets_2017_03 = 
 [
 	{
+		"created_at": "Mon Mar 20 16:23:51 +0000 2017",
+		"id": 843860649063137300,
+		"id_str": "843860649063137280",
+		"text": "Coffee, Fancy Toast, &amp; Art — courtesy @VibrantIllustration #finderscreepers and @youngbloo… https://t.co/imOkSGDIMs https://t.co/qmqcN4HAIs",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "finderscreepers",
+					"indices": [
+						63,
+						79
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "youngbloo",
+					"name": "shawn",
+					"id": 62163388,
+					"id_str": "62163388",
+					"indices": [
+						84,
+						94
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/imOkSGDIMs",
+					"expanded_url": "http://bit.ly/2nWgWI3",
+					"display_url": "bit.ly/2nWgWI3",
+					"indices": [
+						96,
+						119
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 843860647511244800,
+					"id_str": "843860647511244800",
+					"indices": [
+						120,
+						143
+					],
+					"media_url": "http://pbs.twimg.com/media/C7X-ybbWwAAPiX2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C7X-ybbWwAAPiX2.jpg",
+					"url": "https://t.co/qmqcN4HAIs",
+					"display_url": "pic.twitter.com/qmqcN4HAIs",
+					"expanded_url": "https://twitter.com/theredhype/status/843860649063137280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 843860647511244800,
+					"id_str": "843860647511244800",
+					"indices": [
+						120,
+						143
+					],
+					"media_url": "http://pbs.twimg.com/media/C7X-ybbWwAAPiX2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C7X-ybbWwAAPiX2.jpg",
+					"url": "https://t.co/qmqcN4HAIs",
+					"display_url": "pic.twitter.com/qmqcN4HAIs",
+					"expanded_url": "https://twitter.com/theredhype/status/843860649063137280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8498372,
+			"id_str": "8498372",
+			"name": "Michael Ryan Norton",
+			"screen_name": "theredhype",
+			"location": "Seattle, WA",
+			"description": "Startup & Crowdfunding Consultant \n• Facilitator @StartupWeekend \n• Curator @StartupDigest \n• Hustler @StudioCitizen",
+			"url": "http://t.co/1UThaCFcIr",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/1UThaCFcIr",
+							"expanded_url": "http://about.me/MichaelRyanNorton",
+							"display_url": "about.me/MichaelRyanNor…",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1326,
+			"friends_count": 1188,
+			"listed_count": 67,
+			"created_at": "Tue Aug 28 23:40:00 +0000 2007",
+			"favourites_count": 237,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2791,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/24983027/theredhypeTWITbkgd2.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/24983027/theredhypeTWITbkgd2.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/605481768930709504/GupAfhuh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/605481768930709504/GupAfhuh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8498372/1433193248",
+			"profile_link_color": "8A8886",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "8F7961",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Mar 07 18:57:43 +0000 2017",
 		"id": 839188327278374900,
 		"id_str": "839188327278374912",
