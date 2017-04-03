efc9785984e624db4b744b6daeda417deb65231c
michaelryannorton <michael@michaelryannorton.com>
2017-04-02 20:10:19 -0700

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2803,
-	"created_at": "Fri Mar 31 03:10:19 +0000 2017",
+	"tweets": 2804,
+	"created_at": "Mon Apr 03 03:10:18 +0000 2017",
 	"lang": "en"
 }
---
+++ tweets/data/js/tweets/2017_04.js
@@ -0,0 +1,315 @@
+Grailbird.data.tweets_2017_04 = 
+[
+	{
+		"created_at": "Mon Apr 03 01:23:57 +0000 2017",
+		"id": 848707611130302500,
+		"id_str": "848707611130302464",
+		"text": "RT @_amrita_c: Final pitches at #swchicreative with host @theredhype and judges @mkuttan @jimmyodom @arteyvida @recordly_ #exciting #startu…",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "swchicreative",
+					"indices": [
+						32,
+						46
+					]
+				},
+				{
+					"text": "exciting",
+					"indices": [
+						122,
+						131
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "_amrita_c",
+					"name": "amrita",
+					"id": 1936434385,
+					"id_str": "1936434385",
+					"indices": [
+						3,
+						13
+					]
+				},
+				{
+					"screen_name": "theredhype",
+					"name": "Michael Ryan Norton",
+					"id": 8498372,
+					"id_str": "8498372",
+					"indices": [
+						57,
+						68
+					]
+				},
+				{
+					"screen_name": "mkuttan",
+					"name": "matt kuttan",
+					"id": 64427125,
+					"id_str": "64427125",
+					"indices": [
+						80,
+						88
+					]
+				},
+				{
+					"screen_name": "JimmyOdom",
+					"name": "Jimmy Odom",
+					"id": 73549040,
+					"id_str": "73549040",
+					"indices": [
+						89,
+						99
+					]
+				},
+				{
+					"screen_name": "arteyvida",
+					"name": "Amor Montes de Oca",
+					"id": 19078219,
+					"id_str": "19078219",
+					"indices": [
+						100,
+						110
+					]
+				},
+				{
+					"screen_name": "recordly_",
+					"name": "Recordly",
+					"id": 753999190150164500,
+					"id_str": "753999190150164482",
+					"indices": [
+						111,
+						121
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8498372,
+			"id_str": "8498372",
+			"name": "Michael Ryan Norton",
+			"screen_name": "theredhype",
+			"location": "Seattle, WA",
+			"description": "Startup & Crowdfunding Consultant \n• Facilitator @StartupWeekend \n• Curator @StartupDigest \n• Hustler @StudioCitizen",
+			"url": "http://t.co/1UThaCFcIr",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/1UThaCFcIr",
+							"expanded_url": "http://about.me/MichaelRyanNorton",
+							"display_url": "about.me/MichaelRyanNor…",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1326,
+			"friends_count": 1201,
+			"listed_count": 67,
+			"created_at": "Tue Aug 28 23:40:00 +0000 2007",
+			"favourites_count": 238,
+			"utc_offset": -25200,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2797,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/24983027/theredhypeTWITbkgd2.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/24983027/theredhypeTWITbkgd2.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/605481768930709504/GupAfhuh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/605481768930709504/GupAfhuh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8498372/1433193248",
+			"profile_link_color": "8A8886",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "8F7961",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sun Apr 02 23:00:45 +0000 2017",
+			"id": 848671576119795700,
+			"id_str": "848671576119795712",
+			"text": "Final pitches at #swchicreative with host @theredhype and judges @mkuttan @jimmyodom @arteyvida @recordly_… https://t.co/12xg5tLCve",
+			"truncated": true,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "swchicreative",
+						"indices": [
+							17,
+							31
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "theredhype",
+						"name": "Michael Ryan Norton",
+						"id": 8498372,
+						"id_str": "8498372",
+						"indices": [
+							42,
+							53
+						]
+					},
+					{
+						"screen_name": "mkuttan",
+						"name": "matt kuttan",
+						"id": 64427125,
+						"id_str": "64427125",
+						"indices": [
+							65,
+							73
+						]
+					},
+					{
+						"screen_name": "JimmyOdom",
+						"name": "Jimmy Odom",
+						"id": 73549040,
+						"id_str": "73549040",
+						"indices": [
+							74,
+							84
+						]
+					},
+					{
+						"screen_name": "arteyvida",
+						"name": "Amor Montes de Oca",
+						"id": 19078219,
+						"id_str": "19078219",
+						"indices": [
+							85,
+							95
+						]
+					},
+					{
+						"screen_name": "recordly_",
+						"name": "Recordly",
+						"id": 753999190150164500,
+						"id_str": "753999190150164482",
+						"indices": [
+							96,
+							106
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/12xg5tLCve",
+						"expanded_url": "https://twitter.com/i/web/status/848671576119795712",
+						"display_url": "twitter.com/i/web/status/8…",
+						"indices": [
+							108,
+							131
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://www.echofon.com/\" rel=\"nofollow\">Echofon</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1936434385,
+				"id_str": "1936434385",
+				"name": "amrita",
+				"screen_name": "_amrita_c",
+				"location": "Chicago",
+				"description": "software engineer, consumer of all things sweet, care about Bhimpalasi, blues, fractals and doughnuts. Nashik ✈️ New York ✈️ Des Moines ✈️ Chicago",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 166,
+				"friends_count": 512,
+				"listed_count": 10,
+				"created_at": "Sat Oct 05 04:31:00 +0000 2013",
+				"favourites_count": 307,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 142,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/378800000550317610/3ba203477fac7abfdf220ea90dbd501f_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000550317610/3ba203477fac7abfdf220ea90dbd501f_normal.jpeg",
+				"profile_link_color": "91D2FA",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en"
+	}
+]
