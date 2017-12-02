e9456b1b51acbd505824aa1be2171998d95f7c8a
Michael Ryan Norton <michael@michaelryannorton.com>
2017-12-01 19:10:19 -0800

    Added 1 tweets

--- tweets/data/js/payload_details.js
+++ tweets/data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 2822,
-	"created_at": "Sun Nov 26 03:10:18 +0000 2017",
+	"tweets": 2823,
+	"created_at": "Sat Dec 02 03:10:18 +0000 2017",
 	"lang": "en"
 }
---
+++ tweets/data/js/tweets/2017_12.js
@@ -0,0 +1,207 @@
+Grailbird.data.tweets_2017_12 = 
+[
+	{
+		"created_at": "Fri Dec 01 19:51:49 +0000 2017",
+		"id": 936684280981237800,
+		"id_str": "936684280981237760",
+		"text": "San Diego friends, here's the best deal of the year on professional studio recording... https://t.co/IAKbyRiK4F",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IAKbyRiK4F",
+					"expanded_url": "https://twitter.com/StudioCitizen/status/936652298779897856",
+					"display_url": "twitter.com/StudioCitizen/…",
+					"indices": [
+						88,
+						111
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 8498372,
+			"id_str": "8498372",
+			"name": "Michael Ryan Norton",
+			"screen_name": "theredhype",
+			"location": "Seattle, WA",
+			"description": "Startup Consultant • Facilitator @StartupWeekend • Curator @StartupDigest • Hustler @StudioCitizen",
+			"url": "https://t.co/1UThaCWO71",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1UThaCWO71",
+							"expanded_url": "http://about.me/MichaelRyanNorton",
+							"display_url": "about.me/MichaelRyanNor…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1337,
+			"friends_count": 1228,
+			"listed_count": 66,
+			"created_at": "Tue Aug 28 23:40:00 +0000 2007",
+			"favourites_count": 238,
+			"utc_offset": -28800,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2808,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/24983027/theredhypeTWITbkgd2.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/24983027/theredhypeTWITbkgd2.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/605481768930709504/GupAfhuh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/605481768930709504/GupAfhuh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8498372/1433193248",
+			"profile_link_color": "8A8886",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "8F7961",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 936652298779897900,
+		"quoted_status_id_str": "936652298779897856",
+		"quoted_status": {
+			"created_at": "Fri Dec 01 17:44:44 +0000 2017",
+			"id": 936652298779897900,
+			"id_str": "936652298779897856",
+			"text": "Calling all musicians! Get 25% off your studio time at Citizen Recording with this limited time offer... - https://t.co/OjyUpWdinq",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/OjyUpWdinq",
+						"expanded_url": "http://mailchi.mp/9149da42debb/calling-all-musicians",
+						"display_url": "mailchi.mp/9149da42debb/c…",
+						"indices": [
+							107,
+							130
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://www.mailchimp.com\" rel=\"nofollow\">MailChimp</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 493727587,
+				"id_str": "493727587",
+				"name": "Citizen Recording",
+				"screen_name": "StudioCitizen",
+				"location": "San Diego",
+				"description": "",
+				"url": "http://t.co/TqTIk7lo",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/TqTIk7lo",
+								"expanded_url": "http://citizenrecording.com",
+								"display_url": "citizenrecording.com",
+								"indices": [
+									0,
+									20
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 38,
+				"friends_count": 26,
+				"listed_count": 0,
+				"created_at": "Thu Feb 16 03:45:28 +0000 2012",
+				"favourites_count": 0,
+				"utc_offset": -28800,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 73,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/935702225531424768/HzWs40XF_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/935702225531424768/HzWs40XF_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/493727587/1511923584",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en"
+	}
+]
