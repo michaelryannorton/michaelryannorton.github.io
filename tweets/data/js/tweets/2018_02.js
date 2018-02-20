506e369c996dfe1c57b6d3a0ce30a098e8313d00
Michael Ryan Norton <michael@michaelryannorton.com>
2018-02-19 19:10:19 -0800

    Added 1 tweets

--- tweets/data/js/tweets/2018_02.js
+++ tweets/data/js/tweets/2018_02.js
@@ -1,6 +1,107 @@
 Grailbird.data.tweets_2018_02 = 
 [
 	{
+		"created_at": "Tue Feb 20 00:25:04 +0000 2018",
+		"id": 965744075260411900,
+		"id_str": "965744075260411904",
+		"text": "@misskaystarr hi Kay! Mind following back so I can DM ya?",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "misskaystarr",
+					"name": "Kay Starr",
+					"id": 22510576,
+					"id_str": "22510576",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 22510576,
+		"in_reply_to_user_id_str": "22510576",
+		"in_reply_to_screen_name": "misskaystarr",
+		"user": {
+			"id": 8498372,
+			"id_str": "8498372",
+			"name": "Michael Ryan Norton",
+			"screen_name": "theredhype",
+			"location": "San Diego, CA",
+			"description": "Startup & Innovation Consultant • Facilitator @StartupWeekend • Curator @StartupDigest • Hustler @StudioCitizen",
+			"url": "https://t.co/8thqumvT9f",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8thqumvT9f",
+							"expanded_url": "https://mrno.co",
+							"display_url": "mrno.co",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1335,
+			"friends_count": 1243,
+			"listed_count": 66,
+			"created_at": "Tue Aug 28 23:40:00 +0000 2007",
+			"favourites_count": 241,
+			"utc_offset": -28800,
+			"time_zone": "Pacific Time (US & Canada)",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2814,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/24983027/theredhypeTWITbkgd2.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/24983027/theredhypeTWITbkgd2.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/965471352307134465/INydSAaA_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/965471352307134465/INydSAaA_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/8498372/1519021677",
+			"profile_link_color": "8A8886",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "8F7961",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Feb 08 20:25:29 +0000 2018",
 		"id": 961697516092719100,
 		"id_str": "961697516092719104",
